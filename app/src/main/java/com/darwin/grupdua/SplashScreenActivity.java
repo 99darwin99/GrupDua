a8ffd5fb8c70ae2416e75f3369c9ee059eb59ba2
99darwin99 <adrwin23@gmail.com>
2023-07-05 00:12:46 +0700

    Commit Update Proses Pembuatan Aplikasi Grup Dua

---
+++ app/src/main/java/com/darwin/grupdua/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.darwin.grupdua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.darwin.grupdua.MainActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Inisialisasi View
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            }
+        }, 6000);
+    }
+}
